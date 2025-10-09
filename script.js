2c545d1bee8b533e939106985b3df5446ac96488
vuillaume <vitalvuillaume@proton.me>
2025-10-09 22:18:57 +0200

    change

--- script.js
+++ script.js
@@ -2,15 +2,13 @@
   const btn = document.getElementById('btn');
   const resultat = document.getElementById('resultat');
 
-  btn.addEventListener('click', async () => {
-    try {
-      const response = await fetch('https://unapproving-diana-heathless.ngrok-free.dev/exemple', {
-        credentials: 'include' // si besoin
-      });
-      const text = await response.text();
-      resultat.textContent = text;
-
-    } catch (err) {
-      resultat.textContent = 'Erreur : ' + err.message;
-    }
-  });
+    btn.addEventListener('click', () => {
+      fetch(`https://unapproving-diana-heathless.ngrok-free.dev/exemple`)
+        .then(response => response.text())
+        .then(text => {
+          resultat.textContent = text;
+        })
+        .catch(err => {
+          resultat.textContent = 'Erreur : ' + err.message;
+        });
+    });
