1da6073095a88627e5f0947699f169c2f768d94a
vuillaume <vitalvuillaume@proton.me>
2025-10-10 03:09:43 +0200

    change

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
   const resultat = document.getElementById('resultat');
 
     btn.addEventListener('click', () => {
-  fetch('https://concept-pie-criteria-eden.trycloudflare.com/exemple')
+  fetch('https://monserveur.loca.lt/exemple')
     .then(r => r.text())
     .then(t => resultat.textContent = t)
     .catch(e => resultat.textContent = 'Erreur : ' + e.message);
